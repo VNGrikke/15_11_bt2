235310238350b53f136e4961ea618bfe6e64330c
VNGrikke <147390413+VNGrikke@users.noreply.github.com>
2024-01-15 10:58:08 +0700

    Update bt10.js

--- loop/bt10.js
+++ loop/bt10.js
@@ -1,16 +1,16 @@
 let x = +prompt("nhập x");
-let y = "*";
-let t = " " + " " + " " + ;
-console.log(t);
-
-if(x>0 && x%1===0){
-    for(let i = 0; i < x; i++){
-        console.log(y);
-        y += "*"; 
-    }
-    y = "*";
-    let z = t + y;
-    for(let i = x; i > 0; i--){
-        
-    }
+let y = `*`;
+for(i = 0; i < x; i++){
+    console.log(y);
+    y += `*`;
 }
+let z = `*`;
+let doDaiCanLe = x;
+let chuoiDaCanLe = z.padStart(doDaiCanLe, ' '); 
+for(let i = 0; i < x; i++){
+    console.log(chuoiDaCanLe);
+    z += `*`;
+    chuoiDaCanLe = z.padStart(doDaiCanLe, ' '); 
+}
+
+
